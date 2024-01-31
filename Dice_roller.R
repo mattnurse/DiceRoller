fcd001f335044e6ff0790d4977f8e36947e31ac5
Dr Matt Nurse <mattnurse02@gmail.com>
2024-01-31 21:02:13 +1100

    Update Dice_roller.R

--- Dice_roller.R
+++ Dice_roller.R
@@ -3,7 +3,7 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
-# Find out more about building applications with Shiny:
+# Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
 #
